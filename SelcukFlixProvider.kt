c853fa624122623038d78a0958ca4303550b0a7c
Ardac <138930253+ardacy2112@users.noreply.github.com>
2025-12-19 10:29:00 +0300

    Delete SelcukFlixProvider.kt

--- SelcukFlixProvider.kt
+++
@@ -1,7 +0,0 @@
-class SelcukFlixProvider : MainAPI() {
-    override var mainUrl = "https://selcukflix.com"
-    override var name = "SelcukFlix"
-    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
-
-    override val lang = "tr"
-}
