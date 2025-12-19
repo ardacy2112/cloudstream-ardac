ab21d4cecb9da03a06ebc305e4bf954b60c34e6e
Ardac <138930253+ardacy2112@users.noreply.github.com>
2025-12-19 10:29:13 +0300

    Delete HDFilmCehennemiProvider.kt

--- HDFilmCehennemiProvider.kt
+++
@@ -1,7 +0,0 @@
-class HDFilmCehennemiProvider : MainAPI() {
-    override var mainUrl = "https://hdfilmcehennemi.com"
-    override var name = "HDFilmCehennemi"
-    override val supportedTypes = setOf(TvType.Movie, TvType.TvSeries)
-
-    override val lang = "tr"
-}
